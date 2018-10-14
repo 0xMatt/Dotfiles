9e7143869c6dc3db66a1b72c5d605051576fdc9b
0xMatt <matthew.javelet@gmail.com>
2018-10-14 09:43:37 -0500

    Satisfied and stable

--- setup.sh
+++ setup.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-# Make home directories
-mkhomedir_helper $USER
 
 # Fedy
 sudo dnf install -y https://dl.folkswithhats.org/fedora/$(rpm -E %fedora)/RPMS/fedy-release.rpm
@@ -17,8 +15,9 @@ sudo dnf -y install nvidia-driver-libs.i686
 sudo dnf config-manager --add-repo=https://negativo17.org/repos/fedora-steam.repo
 sudo dnf -y install steam
 
-sudo dnf -y install @base-x htop neofetch ncmpcpp transmission-remote-cli transmission-daemon git wget lightdm slick-greeter \
-  weechat compton vim neovim feh adobe-source-code-pro-fonts fontawesome-fonts ranger qutebrowser gnome-terminal zsh util-linux-user
+sudo dnf -y install @base-x htop neofetch ncmpcpp transmission-remote-cli transmission-daemon git wget \
+  weechat compton vim neovim feh adobe-source-code-pro-fonts fontawesome-fonts ranger qutebrowser gnome-terminal \
+  zsh util-linux-user stow whois traceroute 
 
 # Stow
 cd /home/$USER/Dotfiles
@@ -26,15 +25,19 @@ stow compton
 stow i3
 stow polybar
 stow zsh
+stow spacevim
+stow ranger
 cd ~
+
+# Install Antigen
+curl -L git.io/antigen > .antigen.zsh
 
 # Set default shell to zsh
 chsh -s $(which zsh)
 
-curl -L git.io/antigen > .antigen.zsh
-
-sudo systemctl enable lightdm.service
-sudo systemctl set-default graphical.target
+# sudo dnf install -y lightdm slick-greeter
+#sudo systemctl enable lightdm.service
+#sudo systemctl set-default graphical.target
 
 # Copr Repos
 sudo dnf copr enable livegrenier/i3-desktop
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -1,4 +1,5 @@
 export TERM="xterm-256color"
+POWERLEVEL9K_INSTALLATION_PATH=$ANTIGEN_BUNDLES/bhilburn/powerlevel9k
 source ~/.antigen.zsh
 
 # Load the oh-my-zsh's library.
@@ -11,8 +12,7 @@ antigen bundle command-not-found
 antigen bundle arialdomartini/oh-my-git
 antigen bundle history
 antigen bundle laravel
-
-
+antigen bundle chrissicool/zsh-256color
 
 # Syntax highlighting bundle.
 antigen bundle zsh-users/zsh-syntax-highlighting
@@ -21,13 +21,19 @@ antigen bundle zsh-users/zsh-completions
 antigen bundle mafredri/zsh-async
 antigen bundle HeroCC/LS_COLORS
 # Load the theme.
-antigen theme https://github.com/caiogondim/bullet-train-oh-my-zsh-theme bullet-train
 #antigen theme bhilburn/powerlevel9k powerlevel9k
+antigen theme bhilburn/powerlevel9k powerlevel9k
 
 # Powerlevel9k
-#POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(os_icon dir vcs)
-#POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status root_indicator context php_version time)
-#POWERLEVEL9K_MODE='awesome-fontconfig'
+POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(os_icon dir vcs)
+POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status command_execution_time time)
+POWERLEVEL9K_MODE='awesome-fontconfig'
+POWERLEVEL9K_OS_ICON_BACKGROUND="237"
+POWERLEVEL9K_OS_ICON_FOREGROUND="255"
+POWERLEVEL9K_DIR_HOME_FOREGROUND="black"
+POWERLEVEL9K_DIR_HOME_BACKGROUND="068"
+POWERLEVEL9K_DIR_HOME_SUBFOLDER_FOREGROUND="black"
+POWERLEVEL9K_DIR_DEFAULT_FOREGROUND="black"
 
 # Tell Antigen that you're done.
 antigen apply
