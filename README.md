1228fe456686b439d782ebe4a941fddeabebfddf
0xMatt <matthew.javelet@gmail.com>
2018-10-29 22:31:21 -0500

    Refresh

---
+++ README.md
@@ -0,0 +1,9 @@
+# My .files
+
+## Setup script
+
+The setup script is mainly for me to quickly setup a minimal fedora network install. Contains nvidia drivers but other than that has some decent timesavers.
+
+## Screenshot of current commit
+
+![desktop](https://raw.githubusercontent.com/0xMatt/Dotfiles/master/screenshot.png)
--- setup.sh
+++ setup.sh
@@ -1,23 +1,8 @@
 #!/bin/sh
 
-# RPMFusion
-sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+# Make sure dotfiles are dropped in to avoid issues wit stow
+sudo dnf install -y stow
 
-# Chrome
-sudo dnf install -y https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
-
-# Nvidia
-sudo dnf config-manager --add-repo=https://negativo17.org/repos/fedora-nvidia.repo
-sudo dnf -y install nvidia-settings kernel-devel dkms-nvidia nvidia-driver-libs.i686
-
-# Steam
-sudo dnf config-manager --add-repo=https://negativo17.org/repos/fedora-steam.repo
-
-sudo dnf -y install @base-x htop neofetch ncmpcpp transmission-remote-cli transmission-daemon git wget \
-  weechat compton vim neovim feh adobe-source-code-pro-fonts fontawesome-fonts ranger qutebrowser gnome-terminal \
-  zsh util-linux-user stow whois traceroute steam xautolock
-
-# Stow
 cd /home/$USER/.dotfiles
 stow compton
 stow i3
@@ -27,22 +12,35 @@ stow spacevim
 stow ranger
 cd ~
 
+# RPMFusion
+sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+# Chrome
+sudo dnf install -y https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+
+# Nvidia
+sudo dnf config-manager --add-repo=https://negativo17.org/repos/fedora-nvidia.repo
+sudo dnf -y install nvidia-settings kernel-devel dkms-nvidia nvidia-driver-libs.i686 
+
+# Steam
+sudo dnf config-manager --add-repo=https://negativo17.org/repos/fedora-steam.repo
+
+# Default Packages ;)
+sudo dnf -y install @base-x htop neofetch ncmpcpp transmission-remote-cli transmission-daemon git wget \
+  weechat compton vim neovim feh adobe-source-code-pro-fonts fontawesome-fonts ranger qutebrowser gnome-terminal \
+  zsh util-linux-user stow whois traceroute steam xautolock
+
+# Copr Packages
+sudo dnf install -y https://copr-be.cloud.fedoraproject.org/results/valdikss/imgur-screenshot/fedora-rawhide-ppc64le/00751928-imgur-screenshot/imgur-screenshot-1.7.4-1.fc29.noarch.rpm \
+  https://copr-be.cloud.fedoraproject.org/results/livegrenier/i3-desktop/fedora-28-x86_64/00791962-polybar/polybar-3.2.1-1.fc28.x86_64.rpm \
+  https://copr-be.cloud.fedoraproject.org/results/livegrenier/i3-desktop/fedora-28-x86_64/00765535-i3-gaps/i3-gaps-4.15.0.1-1.fc28.x86_64.rpm \
+  https://copr-be.cloud.fedoraproject.org/results/livegrenier/i3-desktop/fedora-28-x86_64/00765537-rofi/rofi-1.5.1-1.fc28.x86_64.rpm
+
 # Install Antigen
 curl -L git.io/antigen > .antigen.zsh
 
 # Set default shell to zsh
 chsh -s $(which zsh)
-
-# sudo dnf install -y lightdm slick-greeter
-#sudo systemctl enable lightdm.service
-#sudo systemctl set-default graphical.target
-
-# Copr Repos
-#sudo dnf copr enable livegrenier/i3-desktop
-#sudo dnf -y install i3-gaps i3lock dmenu polybar
-
-#sudo dnf copr enable valdikss/imgur-screenshot
-#sudo dnf -y install imgur-screenshot
 
 # SpaceVim
 curl -sLf https://spacevim.org/install.sh | bash
