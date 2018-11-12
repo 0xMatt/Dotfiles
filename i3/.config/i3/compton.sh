0943278de77a42c436a4813e26976f8370e46fdd
0xMatt <matthew.javelet@gmail.com>
2018-11-12 15:00:06 -0500

    Update before wipe

--- i3/.config/i3/compton.sh
+++ i3/.config/i3/compton.sh
@@ -1,10 +1,10 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 # Terminate already running bar instances
 killall -q compton 
 
 # Wait until the processes have been shut down
-while pgrep -x compton >/dev/null; do sleep 1; done
+while pgrep -u $UID -x compton >/dev/null; do sleep 1; done
 
-# Launch polybar
-compton -b &
+# Launch
+compton -bC &
