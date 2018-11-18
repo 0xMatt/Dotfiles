fcccb3f915f2c250e83eaa38db6314a60dd6d93d
0xMatt <matthew.javelet@gmail.com>
2018-11-18 10:55:27 -0600

    Make things better

--- development.sh
+++ development.sh
@@ -1,6 +1,26 @@
 # SpaceVim                                                                          
 curl -sLf https://spacevim.org/install.sh | bash
 
-sudo dnf install -y nodejs php php-process php-opcache php-json php-msgpack php-xml php-mbstring php-xdebug
+# PHP Crap
+sudo dnf install -y nodejs php php-process php-opcache php-json php-msgpack php-xml php-mbstring php-xdebug php-intl
 
-sudo npm install -g neovim javascript-typescript-server typescript
+## Composer
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === '93b54496392c062774670ac18b134c3b3a95e5a5e5c8f1a9f115f203b75bf9a129d5daa8ba6a13e2cc8a1da0806388a8') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+chmod +x composer.phar 
+sudo mv composer.phar /usr/bin/composer
+
+wget -O phpunit https://phar.phpunit.de/phpunit-7.phar
+chmod +x phpunit
+sudo mv phpunit /usr/bin/phpunit
+
+# NodeJS/Typescript Crap
+sudo npm install -g javascript-typescript-langserver typescript
+
+## Neo/Vim crap
+sudo npm install -g neovim
+sudo pip2 install neovim
+sudo pip3 install neovim
+
--- setup.sh
+++ setup.sh
@@ -1,15 +1,21 @@
 #!/bin/sh
 
+## Default dirs
+mkdir ~/Downloads ~/Documents ~/Games ~/Code ~/Media
+mkdir ~/Media/Screenshots ~/Media/Wallpapers ~/Media/Music ~/Media/Movies ~/Media/Pictures ~/Media/Videos
+
 # Make sure dotfiles are dropped in to avoid issues wit stow
 sudo dnf install -y stow
 
-cd /home/$USER/.dotfiles
+cd ~/.dotfiles
 stow compton
 stow i3
 stow polybar
 stow zsh
 stow spacevim
 stow ranger
+stow dunst
+stow imgur-screenshot
 cd ~
 
 # RPMFusion
@@ -24,7 +30,7 @@ sudo dnf config-manager --add-repo=https://negativo17.org/repos/fedora-steam.rep
 # Default Packages ;)
 sudo dnf -y install @base-x htop neofetch ncmpcpp transmission-remote-cli transmission-daemon git wget \
   weechat compton vim neovim feh adobe-source-code-pro-fonts fontawesome-fonts ranger qutebrowser gnome-terminal \
-  zsh util-linux-user whois traceroute xautolock dunst steam
+  zsh util-linux-user whois traceroute xautolock dunst steam NetworkManager-wifi
 
 # Copr Repositories
 sudo dnf -y copr enable vladikss/imgur-screenshot
@@ -41,11 +47,8 @@ cd ~
 rm -rf i3lock-fancy
 
 # Install Antigen
-curl -L git.io/antigen > .antigen.zsh
+curl -L git.io/antigen > ~/.antigen.zsh
 
 # Set default shell to zsh
 chsh -s $(which zsh)
-
-# SpaceVim
-curl -sLf https://spacevim.org/install.sh | bash
 
