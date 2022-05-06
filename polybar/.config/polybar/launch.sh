c0a1eccadf10ac8e731bb1115f2e0af0679e4412
0xMatt <matthew.javelet@gmail.com>
2022-05-05 21:38:16 -0500

    new i3 and polybar settings for hp omen 16

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
 # Terminate already running bar instances
+# If all your bars have ipc enabled, you can use 
+#polybar-msg cmd quit
+# Otherwise you can use the nuclear option:
 killall -q polybar
 
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
 # Launch bar1 and bar2
-polybar main &
+echo "---" | tee -a /tmp/polybar-main.log
+polybar main 2>&1 | tee -a /tmp/polybar-main.log & disown
 
-echo "Polybar launched..."
+echo "Bars launched..."
