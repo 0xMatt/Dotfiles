033e8adf973fffcd33167299da0045042fa6b484
0xMatt <matthew.javelet@gmail.com>
2025-06-15 15:09:00 -0500

    Change wallpapers

--- i3/.config/i3/picom.sh
+++ i3/.config/i3/picom.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # Terminate already running bar instances
-killall -q compton 
+killall -q picom
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x compton >/dev/null; do sleep 1; done
+while pgrep -u $UID -x picom >/dev/null; do sleep 1; done
 
 # Launch
-compton -bC &
+picom --config=./.config/picom.conf &
